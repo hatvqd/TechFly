e39cfd1452d94f84b7cbd1074a83a0124e2a4e1b
hatvqd <datvq87@gmail.com>
2016-11-01 23:14:36 +0700

    hi

--- resources/views/templates/blog.blade.php
+++ resources/views/templates/blog.blade.php
@@ -42,13 +42,13 @@
 	            		<span class="post_time">{{ $post->published_date }}</span>
 	            		<blockquote>
 	            			<footer> 
-
+iu
 	            				{!! $post->short_content !!} 
 
 	            				<div style="padding-bottom:20px; padding-top:10px;" class="hupso-share-buttons">
 	            				</div>
 	            			</footer>
-	            			<a href="http://bay5chau.com/ve-may-bay-di-pakse/" class="detail">Xem thêm</a>
+	            			<a href="{{ route('blog.post', [$post->id, $post->;lv ]) }}" class="detail">Xem thêm</a>
 	            		</blockquote>
 	            	</div>
             	</div>
