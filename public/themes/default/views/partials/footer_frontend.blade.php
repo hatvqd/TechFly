2fd4627b0c5841883507d2cbc115094b16fae519
hatvqd <datvq87@gmail.com>
2016-11-09 21:58:33 +0700

    hi

--- public/themes/default/views/partials/footer_frontend.blade.php
+++ public/themes/default/views/partials/footer_frontend.blade.php
@@ -43,6 +43,18 @@
                 <h2 class="title">
                     Chúng tôi trên Facebook
                 </h2>
+
+                 <div id="fb-root"></div>
+        <script>
+            (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=226108041127896";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+        
                 <div class="fb-page" data-href="https://www.facebook.com/techfly.ltd" data-tabs="timeline" data-height="200" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/techfly.ltd">Facebook</a></blockquote></div>
                           
             </div>
